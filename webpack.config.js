28ae09dc5af0ba548ee3f40cd10cfbdf88bf2eb6
Dominic Lee <dominiclee0512@gmail.com>
2019-07-07 15:56:06 +0800

    [Chore] Refactory react app with webpack 4

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  mode: "development",
+  entry: ["./src/index.js"],
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [htmlPlugin],
+  devServer: {
+    port: 3000
+  }
+};
